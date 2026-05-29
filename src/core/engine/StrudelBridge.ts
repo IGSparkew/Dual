86a7d772bab36c42968e013d930c89ba27566588
IGSparkew <40170910+IGSparkew@users.noreply.github.com>
2026-05-29 02:46:46 +0200

    working struddel poc

--- src/core/engine/StrudelBridge.ts
+++ src/core/engine/StrudelBridge.ts
@@ -1,6 +1,7 @@
 import type { Hap } from '../types/hap';
 
 export interface StrudelBridge {
+  init(): Promise<void>;
   evaluate(code: string): Promise<void>;
   queryArc(begin: number, end: number): Hap[];
   dispose(): void;
